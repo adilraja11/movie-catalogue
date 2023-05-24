2d1b2ddf9d830bdf789eb97cd732d8da0e903292
Muhammad Adil Raja Saputra <70511346+adilraja11@users.noreply.github.com>
2023-05-24 10:31:24 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '9d1c47241dad577bf95da2bbe91f0b1d',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
